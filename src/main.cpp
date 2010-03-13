9d8676792f91e0fa14ef3122bd730f161b6c2263
Tymon Tobolski <i@teamon.eu>
2010-03-13 12:21:21 +0100

    tlcd library

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,29 @@
 #include "piast.h"
 #include "usart.h"
 
+#include "common.h"
+#include "tlcd.h"
+
 USART0(usb);
 USART1(zigbit);
 
 int main() {
 	sei();
+	
+	tlcd_init();
+	tlcd_cls();
+	
+	// tlcd_goto(1,1);
+	// tlcd_write_string("Servo control");
+	// tlcd_goto(1,2);
 
 	for(;;){
-		if(!usb.empty()){
-			unsigned char c;
-			usb >> c;
-			usb << c;
-		}
+		// if(!usb.empty()){
+		// 	unsigned char c;
+		// 	usb >> c;
+		// 	usb << c;
+		// }
+		
+		
 	}
 }
